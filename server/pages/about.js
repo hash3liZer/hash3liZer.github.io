b73e23ca72fefc5ffe262e1c4859b7ef82224044
hash3liZer <sheikhshameerkashif@protonmail.com>
2024-09-14 19:36:39 +0000

    Deploying to gh-pages from @ hash3liZer/profileo2@7e95b297e73d763c88887ba1e8bf4f80723e6c23 🚀

---
+++ server/pages/about.js
@@ -0,0 +1,70 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 521;
+exports.ids = [521];
+exports.modules = {
+
+/***/ 4093:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "getStaticProps": () => (/* binding */ getStaticProps),
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+
+const AboutPage = ()=>{
+    return(/*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+        children: [
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h3", {
+                children: "A Little Bit About Me"
+            }),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("br", {
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("p", {
+                children: [
+                    "My profile lies at this link:\xa0",
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                        href: "https://blog.shameerkashif.me/resume",
+                        target: "_blank",
+                        rel: "noopener",
+                        children: "https://blog.shameerkashif.me/resume"
+                    })
+                ]
+            })
+        ]
+    }));
+};
+async function getStaticProps() {
+    return {
+        props: {
+            title: 'About'
+        }
+    };
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (AboutPage);
+
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__(4093));
+module.exports = __webpack_exports__;
+
+})();
